3c436e63c3777c7fe2d29697af46299ebd476e80
InGodWeTrustt <fanat2016@mail.ru>
2023-08-03 10:07:54 +0300

    пару обновлений комментариев

--- package/app.py
+++ package/app.py
@@ -49,6 +49,7 @@ class App(tk.Tk):
 
     def close_window(self, event):
         """Закрытие основного окна приложения"""
+
         isExit = messagebox.askokcancel(
             'Выйти', 'Вы действительно хотите выйти из приложения?')
         if isExit:
@@ -56,6 +57,8 @@ class App(tk.Tk):
             self.destroy()
 
     def config(self, kw):
+        """ Начальная конфигурация """
+
         self.title(kw.get('title'))  # Устанавливаем заголовок окна
         self.geometry(kw.get('size'))  # Устанавливаем размер окна
         # Зкрытие приложения
@@ -78,6 +81,8 @@ class App(tk.Tk):
         self.update_text_finally_price()
 
     def load_data_from_db(self):
+        """ Предварительная загрузка данных из БД прм старте приложения """
+
         data = self.database.get_all_products()
         if data:
             # last elem id
@@ -86,11 +91,14 @@ class App(tk.Tk):
             self.tree.insert_many_elem(data)
 
     def enter_leave_events(self):
+        """ События на кнопке """
+
         self.btn_saved.bind('<Enter>', self.on_enter)
         self.btn_saved.bind('<Leave>', self.on_leave)
 
     def on_enter(self, event):
         """ Функция вызывается, когда курсор мыши наводится на виджет"""
+
         event.widget.config(bg="blue", fg="white")
 
     def on_leave(self, event):
@@ -98,6 +106,7 @@ class App(tk.Tk):
 
     def calc_finally_price(self):
         """ Расчет итоговой стоимости всех товаров в БД"""
+
         total_sum = 0
         for item in self.tree.get_children():
             value = self.tree.item(item)['values'][3]
@@ -106,6 +115,7 @@ class App(tk.Tk):
 
     def update_text_finally_price(self):
         """ Обновляем итоговую цену в метке"""
+        
         self._finally_price = self.calc_finally_price()
         self.finally_price_lbl.config(
             text=f"Итоговая сумма: {str(self.finally_price)} р.")
